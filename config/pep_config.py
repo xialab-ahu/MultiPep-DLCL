ebfc83897d6c592cbb8a248719f2bbd26eb1c326
xialab-ahu <56533532+xialab-ahu@users.noreply.github.com>
2024-06-05 20:37:25 +0800

    Update pep_config.py

--- config/pep_config.py
+++ config/pep_config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time : 2024/5/16 14:35
-# @Author : lt,fhh
+# @Author : lt, fhh
 # @FileName: __init__.py.py
 # @Software: PyCharm
 import argparse
